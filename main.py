4dbeae154107546d2e8a0fb7799a797197660d43
Vitaliy Kudrik <35190787+VitaliyKudrik@users.noreply.github.com>
2021-11-09 00:46:03 -0800

    Updated results page
    Added multiple pages of results and added some extra beautification things.

--- main.py
+++ main.py
@@ -4,7 +4,34 @@ import random
 import datetime
 import requests
 from searcher import anime_searcher
+from math import ceil
+
 app = Flask(__name__)
+
+
+# Helper function to fill the global anime holder properly
+def global_anime_creater(results):
+    global gbl_results
+    gbl_results = {}
+    # Get the amount of pages
+    pages = int((ceil(len(results) / 10) * 10) / 10)
+    # This will keep track of which anime we are on
+    anime_count = 0
+    # This will hold 10 anime which we will add to our global results
+    temp = []
+    # For as many pages of top 10 results that we have
+    for i in range(0, pages):
+        # Get the ten pages for that page
+        for _ in range(0, 10):
+            temp.append(results[anime_count])
+            anime_count += 1
+            # Make sure we stop at the right place
+            if anime_count > len(results) - 1:
+                break
+        # Add the 10 pages to the global results
+        gbl_results[str(i)] = temp
+        # Reset temp for the next 10 pages
+        temp = []
 
 
 # Deal with user queries
@@ -12,41 +39,57 @@ app = Flask(__name__)
 def homepage():
     if request.method == 'POST':
         data = request.form
-        print(data)
     else:
         data = request.args
         x = data.get('search')
-        print(data)
         # Deal with bad user input or no input
         if x is not None and x != "":
             x = x.strip()
             # Send you to the anime you want to see
-            return redirect(f'/search/{x}')
+            return redirect(f'/search/{x}/0')
 
     return render_template('index.html')
 
 
-# Show the actual anime results. Hasn't been made presentable yet
-@app.route('/search/<anime>')
-def search(anime):
-    results = anime_searcher(anime)
-    pics = []
-    for pic in results:
-        pics.append(pic['Picture'])
-
-    return render_template('result.html', name=anime, results=results, pics=pics)
+# Global value to keep track of current searching
+current_search = ""
+gbl_results = {}
 
 
 # Show the actual anime results. Hasn't been made presentable yet
-@app.route('/result/<anime>/<page>')
-def test_search(anime):
-    results = anime_searcher(anime)
-    pics = []
-    for pic in results:
-        pics.append(pic['Picture'])
-    return render_template('result.html', name=anime, results=results, pics=pics, page=page)
+@app.route('/search/<anime>/<page>')
+def search(anime, page):
+    global current_search
+    global gbl_results
+    # Get the amount of pages in the results
+
+    if current_search == "":
+        current_search = anime
+        results = anime_searcher(anime)
+        global_anime_creater(results)
+
+    elif current_search == anime:
+        pass
+    else:
+        current_search = anime
+        results = anime_searcher(anime)
+        global_anime_creater(results)
+    pages = len(gbl_results)
+    curr_page_len = len(gbl_results[page])
+
+    # pics = []
+    # for pic in gbl_results:
+    #     pics.append(pic['Picture'])
+    # I don't think we need more than 48 pages, and it also fits perfectly in our results if there are 48.
+    if pages > 48:
+        pages = 48
+    return render_template('result.html', name=anime, results=gbl_results, pages=pages, page=page, i_page=int(page),
+                           curr_page_len=curr_page_len)
+
+
 
 
 if __name__ == "__main__":
     app.run(debug=True)
+    #app.run()
 
